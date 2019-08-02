917c14f21ded2885d2e945c0691617c3878e1067
Dalwar Hossain (Europe) <dalwar.hossain@dimensiondata.com>
2019-08-02 10:38:01 +0200

    added info about the package

--- dnac_pnp/app.py
+++ dnac_pnp/app.py
@@ -21,6 +21,26 @@ __email__ = "dalwar.hossain@dimensiondata.com"
 init(autoreset=True)
 
 
+# Show information abou the package
+def show_info():
+    """This module prints information about the package on screen"""
+
+    try:
+        from dnac_pnp import __package_name__ as package_name
+        from dnac_pnp import __version__ as version
+        from dnac_pnp import __license__ as package_license
+        from dnac_pnp import __author__ as author
+        from dnac_pnp import __email__ as email
+    except ImportError:
+        print(Fore.RED + f"[x] Can't import information")
+        sys.exit(1)
+    print(Fore.CYAN + f"Package Name: " + Fore.GREEN + f"{package_name}")
+    print(Fore.CYAN + f"Version: " + Fore.GREEN + f"{version}")
+    print(Fore.CYAN + f"License: " + Fore.GREEN + f"{package_license}")
+    print(Fore.CYAN + f"Author: " + Fore.GREEN + f"{author}")
+    print(Fore.CYAN + f"Contact: " + Fore.GREEN + f"{email}")
+
+
 # Validate input serial number
 def validate_alphanumeric(ctx, param, value):
     """This function validates and allows only letters and numbers"""
@@ -113,5 +133,9 @@ def acclaim_in_bulk(catalog_file):
     pass
 
 
-if __name__ == "__main__":
-    mission_control()
+@mission_control.command(short_help="Shows package information.")
+# Information about this package
+def info():
+    """This module prints informaion about the package"""
+
+    show_info()
