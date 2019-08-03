9638ab7fcfe1efc17a2e83c7d9b668e60d537794
Dalwar Hossain (Europe) <dalwar.hossain@dimensiondata.com>
2019-08-03 02:04:40 +0200

    bumpversion 0.1.0 -> 0.1.1
    Signed-off-by: Dalwar Hossain (Europe) <dalwar.hossain@dimensiondata.com>

--- dnac_pnp/__init__.py
+++ dnac_pnp/__init__.py
@@ -6,5 +6,5 @@
 __package_name__ = "dnac_pnp"
 __author__ = """NTT Dimensiondata PS Automation Team"""
 __email__ = "DE.PS.DI.Automation@dimensiondata.com"
-__version__ = "0.1.0"
+__version__ = "__version__ = 0.1.1"
 __license__ = "ntt-eula-1.0.1"
