6efa24366dc10fa2dd4fd3dd1cd469bf790ee37d
Dalwar Hossain (Europe) <dalwar.hossain@dimensiondata.com>
2019-08-24 15:31:45 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://scm.dimensiondata.com/de-ps/automation/carnival/dnac-pnp",
-    version="0.3.1",
+    version="0.3.2",
     zip_safe=False,
 )
